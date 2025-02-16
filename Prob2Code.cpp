8a8d8f184daa5425b16656b406dbb88c122236a5
Kevin Henderson <kevin@Kevins-MacBook-Air-3.local>
2025-02-16 02:38:09 -0800

    Algorithm is done aside from one bugged input

--- Prob2Code.cpp
+++ Prob2Code.cpp
@@ -5,6 +5,9 @@
 // Names:
 // Kevin Henderson, Bryan Tran, Kenny Ly, Nathan Preheim
 //-----------------------------------------------------------------------
+
+// BUGLIST:
+// * input: [2, 0, 4, 1, 3, 5] is producing wrong output.
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,7 +17,7 @@ int main() {
   int row_length = 0;
   int swaps = 0;
   int temp = 0;
-  int temp2 = 0;
+
   // prompt user for length of row
   std::cout << "How long is the row?" << '\n';
   std::cin >> row_length;
@@ -27,16 +30,15 @@ int main() {
     std::cout << "Input must be a number!" << '\n';
     return 1;
   }
-  // create array for row operations
-  int row[row_length];
+
   // create row that doesnt get changed for print later
-  int inprow[row_length];
+  std::vector<int> row;
   // prompt user to add numbers to row
   for (int i = 0; i < row_length; i++) {
     std::cout << "enter number at index " << i << ": ";
-    std::cin >> row[i];
+    std::cin >> temp;
+    row.push_back(temp);
 
-    inprow[i] = row[i];
     // same integer check from above
     if (!std::cin) {
       std::cout << "Input must be a number!" << '\n';
@@ -45,41 +47,33 @@ int main() {
   }
 
   // main swap operation block
-  for (int j = 1; j < row_length; j++) {
-    if (row[j] != row[j - 1] + 1) {
-      // assign the elements to be swapped to temp variables to keep track
-      temp = row[j - 1] + 1;
-      temp2 = row[j];
-      // performs the swap and increments the counter
-      for (int x = 0; x < row_length; x++) {
-        if (row[x] == temp) {
-          row[j] = temp;
-          row[x] = temp2;
-          std::cout << "row[" << j << "]" << " needs to be swapped with row["
-                    << x << "]" << '\n';
+  for (int j = 0; j < row.size(); j++) {
+    // checks to see if individual is sitting next to its partner, if not,
+    // assign whoevers next to it to temp value for swap
+    if (row[j + 1] != row[j] + 1 && row[j + 1] != row[j] - 1) {
+      temp = row[j + 1];
+
+      for (int x = 0; x <= row.size(); x++) {
+        if (row[x] == row[j] + 1 || row[x] == row[j] - 1) {
+          row[j + 1] = row[x];
+          row[x] = temp;
           swaps++;
           break;
         }
       }
     }
+    // skips individual that was just swapped
+    j++;
+  }
+  std::cout << "Number of swaps: " << swaps << '\n';
+
+  // for testing
+  for (int k : row) {
+    std::cout << k << '\n';
   }
 
-  // prints both input row, and newly swapped row and number of swaps performed
-  std::cout << "Input row = [ ";
-  for (int k = 0; k < row_length; k++) {
-    std::cout << inprow[k] << " ";
-  }
-  std::cout << "]" << "\n" << "-----------------------------------" << '\n';
-  std::cout << "Number of swaps: " << swaps << '\n';
   if (swaps == 0) {
     std::cout << "All couples are already seated side by side" << '\n';
-  } else {
-    std::cout << "swapped row = [ ";
-    for (int k = 0; k < row_length; k++) {
-      std::cout << row[k] << " ";
-    }
-    std::cout << "]" << "\n";
   }
-
   return 0;
 }
