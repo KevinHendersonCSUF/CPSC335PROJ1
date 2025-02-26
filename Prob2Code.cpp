4246d348967f77c80a2dd48893278e50fe997a01
Kevin Henderson <kevin@Kevins-MacBook-Air-5.local>
2025-02-25 16:30:52 -0800

    Added the row print and couple check, moved main algorithm into function instead of main

--- Prob2Code.cpp
+++ Prob2Code.cpp
@@ -6,20 +6,47 @@
 // Kevin Henderson, Bryan Tran, Kenny Ly, Nathan Preheim
 //-----------------------------------------------------------------------
 
-// BUGLIST:
-// * input: [2, 0, 4, 1, 3, 5] is producing wrong output.
 #include <iostream>
 #include <string>
 #include <vector>
 
-int main() {
-  int row_input = 0;
-  int row_length = 0;
+std::vector<int> Swapper(std::vector<int>& row) {
+  int temp = 0;
   int swaps = 0;
+  for (int j = 0; j < row.size(); j++) {
+    // checks to see if individual is sitting next to its partner, if not,
+    // assign whoevers next to it to temp value for swap
+    if (row[j + 1] != row[j] + 1 && row[j + 1] != row[j] - 1) {
+      temp = row[j + 1];
+      // iterate through the individuals to find which ones to swap
+      for (int x = 0; x <= row.size(); x++) {
+        if (row[x] == row[j] + 1 || row[x] == row[j] - 1) {
+          row[j + 1] = row[x];
+          row[x] = temp;
+          swaps++;
+          // prints what individuals are swapped
+          std::cout << "row[" << j + 1 << "] must be swapped with row[" << x
+                    << "]" << '\n';
+          break;
+        }
+      }
+    }
+    // skips individual that was just swapped
+    j++;
+  }
+  std::cout << "Number of swaps: " << swaps << '\n';
+  if (swaps == 0) {
+    std::cout << "All couples are already seated side by side" << '\n';
+  }
+  return row;
+}
+
+int main() {
+  int row_length = 0;
   int temp = 0;
 
   // prompt user for length of row
-  std::cout << "How long is the row?" << '\n';
+  std::cout << "How long is the row: ";
   std::cin >> row_length;
   if (row_length > 30 || row_length % 2 != 0) {
     std::cout << "Row must be less than 30 and even!" << '\n';
@@ -31,7 +58,6 @@ int main() {
     return 1;
   }
 
-  // create row that doesnt get changed for print later
   std::vector<int> row;
   // prompt user to add numbers to row
   for (int i = 0; i < row_length; i++) {
@@ -45,35 +71,33 @@ int main() {
       return 1;
     }
   }
+  std::cout << "-----------------------------------" << '\n';
+  // perform the swap
+  Swapper(row);
 
-  // main swap operation block
-  for (int j = 0; j < row.size(); j++) {
-    // checks to see if individual is sitting next to its partner, if not,
-    // assign whoevers next to it to temp value for swap
-    if (row[j + 1] != row[j] + 1 && row[j + 1] != row[j] - 1) {
-      temp = row[j + 1];
-
-      for (int x = 0; x <= row.size(); x++) {
-        if (row[x] == row[j] + 1 || row[x] == row[j] - 1) {
-          row[j + 1] = row[x];
-          row[x] = temp;
-          swaps++;
-          break;
-        }
-      }
+  // print the row after swap
+  std::cout << "[";
+  int pair = 0;
+  for (int k = 0; k < row.size(); k++) {
+    if (pair == 0) {
+      std::cout << "(";
     }
-    // skips individual that was just swapped
-    j++;
+    std::cout << row[k];
+    pair++;
+    if (pair == 1) {
+      std::cout << ", ";
+    }
+    if (pair == 2) {
+      //check if couples follow the (2n-2, 2n-1) format stated in project instructions, if not, stop printing and print error message
+      if(row[k-1] != row[k] + 1 && row[k-1] != row[k] - 1){
+        std::cout << '\n' << "Couples must follow (2n-2, 2n-1)!" << '\n';
+        return 1;
+      }
+      std::cout << ")";
+      pair = 0;
+    }
   }
-  std::cout << "Number of swaps: " << swaps << '\n';
+  std::cout << "]" << '\n';
 
-  // for testing
-  for (int k : row) {
-    std::cout << k << '\n';
-  }
-
-  if (swaps == 0) {
-    std::cout << "All couples are already seated side by side" << '\n';
-  }
   return 0;
 }
